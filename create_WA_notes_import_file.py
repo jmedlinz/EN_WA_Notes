8890d2e3a2569dc25b26c09e46b617e955333c05
James <james@themedlins.com>
2023-02-06 10:28:20 -0600

    First draft

---
+++ create_WA_notes_import_file.py
@@ -0,0 +1,32 @@
+from datetime import date, datetime
+from pprint import pprint
+import os
+# from calendar import monthrange
+import document
+
+def main():
+
+    FOLDER = r'c:\temp'
+
+    # Ask the user which month and user they'd like to use.
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+    current_month_string = datetime.now().strftime("%B")
+    year = int(input(f"What year? (ENTER for {current_year})") or current_year)
+    month = int(input(f"What month? (ENTER for {current_month_string})") or current_month)
+
+    # Creating a document of WA notes for the chosen month and year.
+    doc = document.Document(year, month)
+
+    # Write the new document to a file
+    # WA_notes.YYYYMM.enex
+    filename = f'WA_notes.{year}{str(month).zfill(2)}.enex'
+    fullpath = os.path.join(FOLDER, filename)
+    f = open(fullpath, "w")
+    f.write(doc.content())
+    f.close()
+
+    print(f'The file was saved in the file "{fullpath}".')
+
+
+main()
---
+++ document.py
@@ -0,0 +1,25 @@
+# from datetime import date, datetime #, timedelta, datetime
+from calendar import monthrange
+import note
+
+class Document:
+
+    def __init__(self, year, month):
+
+        HEADER  = '<?xml version="1.0" encoding="UTF-8"?>\n'
+        HEADER += '<!DOCTYPE en-export SYSTEM "http://xml.evernote.com/pub/evernote-export4.dtd">\n'
+        HEADER += '<en-export export-date="20220501T070608Z" application="Evernote" version="10.33.5">\n'
+
+        FOOTER  = '</en-export>\n'
+
+        # Create a list of notes, one for each day of the month.
+        notes = [note.Note(year, month, i+1).content() for i in range(monthrange(year, month)[1])]
+
+        # Assemble the header, notes, and footer into a document.
+        self.__doc = HEADER + ''.join(notes) + FOOTER
+
+    def content(self):
+        return self.__doc
+
+    def __repr__(self):
+        return self.__doc
